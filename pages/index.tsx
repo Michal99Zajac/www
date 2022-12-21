c9e62f460e3606efa8365331fbfd73e5edbbe844
Michal99Zajac <michal.99.zajac@gmail.com>
2022-12-21 19:53:23 +0100

    feat(app): change Michał to Michal

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,27 +15,28 @@ const Home: NextPageWithLayout = () => {
       <Head>
         <title>Michal Zajac - Home</title>
       </Head>
-      <section
-        id="intro"
-        className="flex max-h-[1080px] items-center justify-center bg-white py-10 px-4 sm:h-screen"
-      >
-        <div className="max-w-[400px] border-[1px] border-gray-200 bg-gray-50 p-4 drop-shadow-lg">
-          <div className="image-card aspect-square w-full">
-            <Image
-              src="/photo/me.jpg"
-              alt="my photo"
-              width={300}
-              height={300}
-              className="h-full w-full object-cover"
-            />
+      <main>
+        <section
+          id="intro"
+          className="flex max-h-[1080px] items-center justify-center bg-white py-10 px-4 sm:h-screen"
+        >
+          <div className="max-w-[400px] border-[1px] border-gray-200 bg-gray-50 p-4 drop-shadow-lg">
+            <div className="image-card aspect-square w-full">
+              <Image
+                src="/photo/me.jpg"
+                alt="my photo"
+                width={300}
+                height={300}
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className="pt-4">
+              <h1 className="font-gloria text-xl">Hi!</h1>
+              <h2 className="font-gloria">My name is Michal and I am a software engineer.</h2>
+            </div>
           </div>
-          <div className="pt-4">
-            <h1 className="font-gloria text-xl">Hi!</h1>
-            <h2 className="font-gloria">My name is Michał and I am a software engineer.</h2>
-          </div>
-        </div>
-      </section>
-      {/* <section id="about" className="flex justify-center bg-white py-20 px-4">
+        </section>
+        {/* <section id="about" className="flex justify-center bg-white py-20 px-4">
         <div className="sm:w-[400px]">
           <h1 className="mb-4 font-gloria text-3xl">About me</h1>
           <p className="text-justify">
@@ -60,100 +61,101 @@ const Home: NextPageWithLayout = () => {
           </p>
         </div>
       </section> */}
-      <section id="skills" className="flex flex-col bg-black py-20 px-4 sm:items-center">
-        <h1 className="mb-16 font-gloria text-5xl text-white sm:text-6xl">My skills!</h1>
-        <div className="grid grid-cols-1 gap-12 sm:w-[500px] sm:grid-cols-2 sm:gap-4 md:w-[1000px] md:grid-cols-4">
-          <div>
-            <h1 className="mb-8 font-gloria text-4xl text-white">Technologies</h1>
-            <h2 className="mb-6 font-gloria text-2xl text-white">Languages</h2>
-            <ul>
-              {skill['programmingLanguages'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-            </ul>
-            <h2 className="mt-8 mb-6 font-gloria text-2xl text-white">Databases</h2>
-            <ul>
-              {skill['databases'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-            </ul>
+        <section id="skills" className="flex flex-col bg-black py-20 px-4 sm:items-center">
+          <h1 className="mb-16 font-gloria text-5xl text-white sm:text-6xl">My skills!</h1>
+          <div className="grid grid-cols-1 gap-12 sm:w-[500px] sm:grid-cols-2 sm:gap-4 md:w-[1000px] md:grid-cols-4">
+            <div>
+              <h1 className="mb-8 font-gloria text-4xl text-white">Technologies</h1>
+              <h2 className="mb-6 font-gloria text-2xl text-white">Languages</h2>
+              <ul>
+                {skill['programmingLanguages'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+              </ul>
+              <h2 className="mt-8 mb-6 font-gloria text-2xl text-white">Databases</h2>
+              <ul>
+                {skill['databases'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h1 className="mb-8 font-gloria text-4xl text-white">Frameworks</h1>
+              <ul>
+                {skill['frameworks'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h1 className="mb-8 font-gloria text-4xl text-white">Tools</h1>
+              <ul>
+                {skill['tools'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h1 className="mb-8 font-gloria text-4xl text-white">Other</h1>
+              <ul>
+                {skill['other'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+                <h2 className="mt-8 mb-6 font-gloria text-2xl text-white">Languages</h2>
+                {skill['languages'].map((skill) => (
+                  <SkillItem
+                    key={skill.skill}
+                    level={skill.level}
+                    skill={skill.skill}
+                    img={skill.img}
+                  />
+                ))}
+              </ul>
+            </div>
           </div>
-          <div>
-            <h1 className="mb-8 font-gloria text-4xl text-white">Frameworks</h1>
-            <ul>
-              {skill['frameworks'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-            </ul>
+        </section>
+        <section id="projects" className="flex flex-col items-center bg-white py-20 px-4">
+          <h1 className="mb-16 font-gloria text-5xl sm:text-6xl">My projects!</h1>
+          <div className="grid max-w-[1000px] grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+            {projects.map((project) => (
+              <ProjectPhoto
+                key={project.title}
+                img={project.img}
+                title={project.title}
+                description={project.description}
+                language={project.language}
+                ghUrl={project.ghUrl}
+                url={project.url}
+              />
+            ))}
           </div>
-          <div>
-            <h1 className="mb-8 font-gloria text-4xl text-white">Tools</h1>
-            <ul>
-              {skill['tools'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h1 className="mb-8 font-gloria text-4xl text-white">Other</h1>
-            <ul>
-              {skill['other'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-              <h2 className="mt-8 mb-6 font-gloria text-2xl text-white">Languages</h2>
-              {skill['languages'].map((skill) => (
-                <SkillItem
-                  key={skill.skill}
-                  level={skill.level}
-                  skill={skill.skill}
-                  img={skill.img}
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
-      </section>
-      <section id="projects" className="flex flex-col items-center bg-white py-20 px-4">
-        <h1 className="mb-16 font-gloria text-5xl sm:text-6xl">My projects!</h1>
-        <div className="grid max-w-[1000px] grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-          {projects.map((project) => (
-            <ProjectPhoto
-              key={project.title}
-              img={project.img}
-              title={project.title}
-              description={project.description}
-              language={project.language}
-              ghUrl={project.ghUrl}
-              url={project.url}
-            />
-          ))}
-        </div>
-      </section>
+        </section>
+      </main>
       <footer className="flex justify-center bg-black py-20 px-4">
         <div className="w-full max-w-[1000px]">
           <h1 className="font-gloria text-2xl text-white">Contact</h1>
