60baa2258e5905eb3039c6056f14ed66d5dbeda8
Michal99Zajac <michal.99.zajac@gmail.com>
2023-12-04 10:56:26 +0100

    feat(page): start developing webiste

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="font-hermeneus text-[160px]">PORTFOLIO</h1>
-    </main>
+    <>
+      <main className="w-screen overflow-x-hidden">
+        <section id="preambula" className=" h-screen bg-blue-500">
+          <div className="container mx-auto h-[400px] w-full p-8">
+            <div className="flex">
+              <div className="h-24 w-24 border-2 border-dashed border-white"></div>
+              <div className="h-24 w-24 border-2 border-l-0 border-dashed border-white"></div>
+            </div>
+          </div>
+        </section>
+        <section id="resume"></section>
+        <section id="experience"></section>
+        <section id="skills"></section>
+        <section id="projects"></section>
+      </main>
+      <footer></footer>
+    </>
   )
 }
