0734e24b2a550197e649263918edb28dc1eed7b4
Michal99Zajac <michal.99.zajac@gmail.com>
2023-12-11 21:23:42 +0100

    feat(ResumeWireIcon): remove broken shadow

--- icons/ResumeWire/index.tsx
+++ icons/ResumeWire/index.tsx
@@ -18,42 +18,9 @@ export function ResumeWireIcon(props: IconProps) {
         strokeWidth="2"
         strokeDasharray="4 4"
       />
-      <circle cx="1014" cy="327" r="10" fill="#3B82F6" />
+      <circle cx="1014" cy="327" r="10" fill="#3B82F6" className="pulse-circle" />
       <circle cx="898" cy="326" r="6" fill="#3B82F6" />
       <circle cx="6" cy="326" r="6" fill="#3B82F6" />
-      <defs>
-        <filter
-          id="filter0_d_180_6"
-          x="1000"
-          y="313"
-          width="28"
-          height="28"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feMorphology
-            radius="4"
-            operator="dilate"
-            in="SourceAlpha"
-            result="effect1_dropShadow_180_6"
-          />
-          <feOffset />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0.231373 0 0 0 0 0.509804 0 0 0 0 0.964706 0 0 0 0.25 0"
-          />
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_180_6" />
-          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_180_6" result="shape" />
-        </filter>
-      </defs>
     </svg>
   )
 }
