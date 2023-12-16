196f7fde639069119a241ba09699b1d90e49262f
Michal99Zajac <michal.99.zajac@gmail.com>
2023-12-16 20:49:20 +0100

    feat(layout): set width of the body to 100vw

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden">
+      <body className="w-screen overflow-x-hidden">
         {children}
         <SpeedInsights />
       </body>
