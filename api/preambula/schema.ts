8c3bdb749144deced4b6177c3fe40817ef4568a8
Michal99Zajac <michal.99.zajac@gmail.com>
2023-12-21 18:48:58 +0100

    refactor(api): update api schemas

--- api/preambula/schema.ts
+++ api/preambula/schema.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+import { imageSchema } from '@/schemas/imageSchema'
+
 /**
  * Preambula schema
  */
@@ -27,75 +29,8 @@ export const preambulaGETSchema = z.object({
         metaViewport: z.string().nullable(),
         canonicalURL: z.string().nullable(),
       }),
-      images: z.object({
-        data: z.array(
-          z.object({
-            id: z.number(),
-            attributes: z.object({
-              name: z.string(),
-              alternativeText: z.string().nullable(),
-              caption: z.string().nullable(),
-              width: z.number(),
-              height: z.number(),
-              formats: z.object({
-                large: z.object({
-                  ext: z.string(),
-                  url: z.string(),
-                  hash: z.string(),
-                  mime: z.string(),
-                  name: z.string(),
-                  path: z.string().nullable(),
-                  size: z.number(),
-                  width: z.number(),
-                  height: z.number(),
-                }),
-                medium: z.object({
-                  ext: z.string(),
-                  url: z.string(),
-                  hash: z.string(),
-                  mime: z.string(),
-                  name: z.string(),
-                  path: z.string().nullable(),
-                  size: z.number(),
-                  width: z.number(),
-                  height: z.number(),
-                }),
-                small: z.object({
-                  ext: z.string(),
-                  url: z.string(),
-                  hash: z.string(),
-                  mime: z.string(),
-                  name: z.string(),
-                  path: z.string().nullable(),
-                  size: z.number(),
-                  width: z.number(),
-                  height: z.number(),
-                }),
-                thumbnail: z.object({
-                  ext: z.string(),
-                  url: z.string(),
-                  hash: z.string(),
-                  mime: z.string(),
-                  name: z.string(),
-                  path: z.string().nullable(),
-                  size: z.number(),
-                  width: z.number(),
-                  height: z.number(),
-                }),
-              }),
-              hash: z.string(),
-              ext: z.string(),
-              mime: z.string(),
-              size: z.number(),
-              url: z.string(),
-              previewUrl: z.string().nullable(),
-              provider: z.string(),
-              provider_metadata: z.string().nullable(),
-              createdAt: z.string().transform((date) => new Date(date)),
-              updatedAt: z.string().transform((date) => new Date(date)),
-            }),
-          }),
-        ),
+      image: z.object({
+        data: imageSchema,
       }),
     }),
   }),
