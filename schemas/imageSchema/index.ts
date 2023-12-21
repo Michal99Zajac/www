b82849b5b6bde7548124c1d5d7aeff81a8fc8dc2
Michal99Zajac <michal.99.zajac@gmail.com>
2023-12-21 18:48:03 +0100

    feat(schemas): add imageSchema

---
+++ schemas/imageSchema/index.ts
@@ -0,0 +1,73 @@
+import { z } from 'zod'
+
+/**
+ * Image schema
+ */
+export const imageSchema = z.object({
+  id: z.number(),
+  attributes: z.object({
+    name: z.string(),
+    alternativeText: z.string().nullable(),
+    caption: z.string().nullable(),
+    width: z.number(),
+    height: z.number(),
+    formats: z.object({
+      large: z.object({
+        ext: z.string(),
+        url: z.string(),
+        hash: z.string(),
+        mime: z.string(),
+        name: z.string(),
+        path: z.string().nullable(),
+        size: z.number(),
+        width: z.number(),
+        height: z.number(),
+      }),
+      medium: z.object({
+        ext: z.string(),
+        url: z.string(),
+        hash: z.string(),
+        mime: z.string(),
+        name: z.string(),
+        path: z.string().nullable(),
+        size: z.number(),
+        width: z.number(),
+        height: z.number(),
+      }),
+      small: z.object({
+        ext: z.string(),
+        url: z.string(),
+        hash: z.string(),
+        mime: z.string(),
+        name: z.string(),
+        path: z.string().nullable(),
+        size: z.number(),
+        width: z.number(),
+        height: z.number(),
+      }),
+      thumbnail: z.object({
+        ext: z.string(),
+        url: z.string(),
+        hash: z.string(),
+        mime: z.string(),
+        name: z.string(),
+        path: z.string().nullable(),
+        size: z.number(),
+        width: z.number(),
+        height: z.number(),
+      }),
+    }),
+    hash: z.string(),
+    ext: z.string(),
+    mime: z.string(),
+    size: z.number(),
+    url: z.string(),
+    previewUrl: z.string().nullable(),
+    provider: z.string(),
+    provider_metadata: z.string().nullable(),
+    createdAt: z.string().transform((date) => new Date(date)),
+    updatedAt: z.string().transform((date) => new Date(date)),
+  }),
+})
+
+export type ImageSchema = z.infer<typeof imageSchema>
