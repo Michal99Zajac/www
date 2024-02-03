9f340355fc8c1cfc97760e9fedd29094c29bf7d8
Michal99Zajac <michal.99.zajac@gmail.com>
2024-02-03 23:29:41 +0100

    feat(env): remove API_URL

--- config/env/index.ts
+++ config/env/index.ts
@@ -4,10 +4,6 @@ export const env = cleanEnv(process.env, {
   API_TOKEN: str({
     desc: 'The API token used to authenticate requests to the API',
   }),
-  API_URL: url({
-    desc: 'The URL of the API',
-    example: 'https://api.example.com/api',
-  }),
   API_GRAPHQL_URL: url({
     desc: 'The URL of the GraphQL API',
     example: 'https://api.example.com/graphql',
